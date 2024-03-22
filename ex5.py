d401c87964de5d54e651ca532a4b2eaf947f3b0d
Aroush Qureshi <aroushq1@gmail.com>
2024-03-22 14:30:25 -0600

    exercise 5 part 1

---
+++ ex5.py
@@ -0,0 +1,5 @@
+""" 
+    The Depth-First Search algorithm is used to implement topological sorting.
+    It is used because it can efficiently explore all edges and vertices of a given graph in order to find the complete
+    dependency structure among vertices (nodes).
+"""
